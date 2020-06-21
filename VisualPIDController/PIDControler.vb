06edc368bcf6ca9acd58722ac09068a1a58d7233
Felix S <fsimmet@gmx.net>
2020-06-21 14:17:30 +0200

    Update PIDControler.vb

--- VisualPIDController/PIDControler.vb
+++ VisualPIDController/PIDControler.vb
@@ -1,20 +1,20 @@
 ﻿Public Class PIDControler
-	Public Property Kp As Double
-	Public Property Ki As Double
-	Public Property Kd As Double
+	Public Property kP As Double = 1.0
+	Public Property kI As Double = 0.0
+	Public Property kD As Double = 0.0
 
 	Public Property SetPointValue As Double
 	Public Property ProcessValue As Double
 	Public Property ControlValue As Double
 	Public Property Interval As Double
 
-	Public Property AntiWindUpEnable As Boolean
-	Public Property AntiWindUpMin As Double
-	Public Property AntiWindUpMax As Double
+	Public Property AntiWindUpEnable As Boolean = False
+	Public Property AntiWindUpMin As Double = -10.0
+	Public Property AntiWindUpMax As Double = +10.0
 
-	Public Property ControlValueLimitEnable As Boolean
-	Public Property ControlValueMin As Double
-	Public Property ControlValueMax As Double
+	Public Property ControlValueLimitEnable As Boolean = False
+	Public Property ControlValueMin As Double = -10.0
+	Public Property ControlValueMax As Double = +10.0
 
 	Public Property mP As Double
 	Public Property mI As Double
@@ -24,21 +24,15 @@
 	Private swt As StopwatchTimer
 
 	Public Sub New()
-		Me.Kp = 1.0
-		Me.Ki = 0.0
-		Me.Kd = 0.0
-
-		Me.Interval = 1
-
-		Me.AntiWindUpEnable = False
-		Me.AntiWindUpMin = -10.0
-		Me.AntiWindUpMax = +10.0
-
-		Me.ControlValueLimitEnable = False
-		Me.ControlValueMin = -10.0
-		Me.ControlValueMax = +10.0
-
 		Me.swt = New StopwatchTimer
+	End Sub
+
+	Public Sub New(kP As Double, kI As Double, kD As Double)
+		Me.New()
+
+		Me.kP = kP
+		Me.kI = kI
+		Me.kD = kD
 	End Sub
 
 	Public Sub UpdateInterval()
@@ -52,23 +46,30 @@
 	End Sub
 
 	Public Sub Calculate()
+		'Proportional
 		Me.mP = Me.SetPointValue - Me.ProcessValue
 
+		'Integral
 		Me.mI = Me.mI + (Me.mP * Me.Interval)
 		If Me.AntiWindUpEnable Then
 			If Me.mI < Me.AntiWindUpMin Then Me.mI = Me.AntiWindUpMin
 			If Me.mI > Me.AntiWindUpMax Then Me.mI = Me.AntiWindUpMax
 		End If
 
+		'Derivative
 		Me.mD = (Me.mP - Me.mPLast) / Me.Interval
 
-		Me.mPLast = Me.mP
-		Me.ControlValue = (Me.mP * Me.Kp) + (Me.mI * Me.Ki) + (Me.mD * Me.Kd)
+		'calculate control value
+		Me.ControlValue = (Me.mP * Me.kP) + (Me.mI * Me.kI) + (Me.mD * Me.kD)
 
+		'limit control value
 		If Me.ControlValueLimitEnable Then
 			If Me.ControlValue < Me.ControlValueMin Then Me.ControlValue = Me.ControlValueMin
 			If Me.ControlValue > Me.ControlValueMax Then Me.ControlValue = Me.ControlValueMax
 		End If
+
+		'save last P value
+		Me.mPLast = Me.mP
 	End Sub
 
 	Public Sub Calculate(SetPoint As Double)
@@ -77,23 +78,17 @@
 	End Sub
 
 	Public Sub Calculate(SetPoint As Double, ProcessValue As Double)
-		Me.SetPointValue = SetPoint
 		Me.ProcessValue = ProcessValue
-		Me.Calculate()
+		Me.Calculate(SetPoint)
 	End Sub
 
 	Public Sub Calculate(SetPoint As Double, ProcessValue As Double, ControlValue As Double)
-		Me.SetPointValue = SetPoint
-		Me.ProcessValue = ProcessValue
 		Me.ControlValue = ControlValue
-		Me.Calculate()
+		Me.Calculate(SetPoint, ProcessValue)
 	End Sub
 
-	Public Sub Calculate(SetPoint As Double, ActualValue As Double, ControlValue As Double, Interval As Double)
-		Me.SetPointValue = SetPoint
-		Me.ProcessValue = ActualValue
-		Me.ControlValue = ControlValue
+	Public Sub Calculate(SetPoint As Double, ProcessValue As Double, ControlValue As Double, Interval As Double)
 		Me.Interval = Interval
-		Me.Calculate()
+		Me.Calculate(SetPoint, ProcessValue, ControlValue)
 	End Sub
 End Class
